e30fb077c683f8c75e38d1d713b6ace9e2e71698
Jonas Wehrung-Montpezat <94191158+ProfesseurShadoko@users.noreply.github.com>
2024-11-05 19:33:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 
-from fancy_string import cstr
-from message import Message
-from progress_bar import ProgressBar
-from task import Task
+from .fancy_string import cstr
+from .message import Message
+from .progress_bar import ProgressBar
+from .task import Task
