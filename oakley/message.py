fb34167197d38ecdc37df06535db16b3b8231d5a
Kiwi <professeur.shadoko1984@gmail.com>
2025-11-07 17:57:59 +0100

    message fancy listing

--- oakley/message.py
+++ oakley/message.py
@@ -71,6 +71,46 @@ class Message(MutableClass):
         Prints the current working directory.
         """
         Message(f"Current working directory: {cstr(os.getcwd()):g}", "#")
+        
+    def list(self, collection:list|dict) -> None:
+        """
+        Displays the content of the list by printing them one by one. If the object is a dictionary, it prints the keys and values.
+        """
+        color = {
+            "#": "g",
+            "?": "y",
+            "i": "c",
+            "!": "r"
+        }[self.type]
+        with Message.tab():
+            
+            n_digits = None
+            if not isinstance(collection, dict):
+                # check that colleciton is iterable
+                try:
+                    iter(collection)
+                except TypeError:
+                    Message.print(collection) # just print out the object
+                
+                # transform into a dictionary idx --> value
+                collection = {i: value for i, value in enumerate(collection)}
+                n_digits = len(str(len(collection)-1)) # optimized computation of log10 here
+                
+            if len(collection) == 0:
+                Message.print(f"{cstr('empty'):ri}")
+            
+            # find the longest key in the collection
+            max_key_length = max([len(str(key)) for key in collection]) if n_digits is None else n_digits
+            
+            for key, value in collection.items():
+                
+                if n_digits is None:
+                    key = f"{cstr(key):{color}}:" + " " * (max_key_length - len(str(key)))
+                else:
+                    key = f"{cstr(key, f'0{n_digits}d'):{color}}:"
+                
+                Message.print(f"{key} {value}")
+            
 
 
 
@@ -92,6 +132,15 @@ if __name__ == '__main__':
     Message("This message should not be indented.")
     Message.par()
     
+    
+    my_array = [1, 2, 0, 0, 89, 1]
+    my_dict = {
+        "name": "Bob",
+        "age": 21
+    }
+    
+    Message("My Array:", "?").list(my_array)
+    Message("Information:").list(my_dict)
     
     
     
